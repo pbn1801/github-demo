0eb6bc320f30d2c73e69696019ba7f375184d325
Nguyen Pham <nguyenpham9758@gmail.com>
2024-05-31 23:25:31 +0700

    Update main.js file

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
 console.log("Hello world")
-console.log("Hello world")
-console.log("Hello world")
-console.log("Hello world")
-console.log("Hello world")
 
-console.log("My name is Nguyen")
-console.log("I am 21 years old")
